a7affc25116c99651066514b70465b2747948d77
TotoroPHD <64203596+TotoroPHD@users.noreply.github.com>
2022-11-10 16:32:56 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,34 @@
+var nomChaineTwitch = 'TotoroPHD'  // METTEZ ICI LE NOM DE VOTRE CHAINE TWITCH
+
+var googleMaps =
+    [
+        // Round 1 : 
+        '<iframe src="https://www.google.com/maps/embed?pb=!4v1668032969416!6m8!1m7!1sRSVzAppaX_ITbwFHP8XNBw!2m2!1d39.24420790963297!2d16.54800527578782!3f60.97941790749783!4f0.2563914996873393!5f0.7820865974627469" width="1720" height="1080" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        // Round 2 : 
+        '<iframe src="https://www.google.com/maps/embed?pb=!4v1668033034633!6m8!1m7!1szVSOSB34oPGDV2VLxpOPZg!2m2!1d5.023999218177508!2d-74.0050350130003!3f295.78355463927466!4f7.508964159255584!5f0.7820865974627469" width="1720" height="1080" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        // Round 3 : 
+        '<iframe src="https://www.google.com/maps/embed?pb=!4v1668033112535!6m8!1m7!1srQFB6-P_-MKJaFaWrUYNzw!2m2!1d23.69315317205257!2d90.43383888749545!3f141.3947!4f0!5f0.7820865974627469" width="1720" height="1080" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        // Round 4 :
+        '<iframe src="https://www.google.com/maps/embed?pb=!4v1668033159542!6m8!1m7!1sXAmSMiEw36zSsOQO9nxddQ!2m2!1d-0.444755820265375!2d36.97583812800001!3f186.51127351449344!4f0.8941221602334224!5f0.7820865974627469" width="1720" height="1080" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>',
+        // Round 5 : 
+        '<iframe src="https://www.google.com/maps/embed?pb=!4v1668033245111!6m8!1m7!1sXaSmQBZlEd86fk1iYByXCQ!2m2!1d56.95756131891078!2d24.18953365922986!3f73.86868520074125!4f0.09349177793050956!5f0.7820865974627469" width="1720" height="1080" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>'
+    ]
+
+var listePays =
+    [
+        'Italie',
+        'Colombie',
+        'Bangladesh',
+        'Kenya',
+        'Lettonie'
+    ]
+
+var displayRound = true;
+
+var color5000 = "red";
+var color4000 = "orange";
+var color3000 = "yellow";
+var color2000 = "#96a832";
+var color1000 = "#64a832";
+var color1 = "#32a851";
+var color0 = "green";
